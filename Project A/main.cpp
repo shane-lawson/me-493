9ab5bedb62a40cdb9022a84355c46f6e5a7a2054
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-08 22:37:36 -0800

    Initial testing Arm

---
+++ Project A/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  Project A
+//
+//  Created by Shane Lawson on 2/7/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include "MAB.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+   // insert code here...
+   cout << "Magical butterflies and unicorns." << endl;
+   
+   Arm arm1;
+   cout << "Mean: " << arm1.mean << "\tstDev: " << arm1.stdDev << endl;
+   
+   int num = 1000;
+   
+   double rewards[num];
+   
+   for (int i = 0; i < num; i++) {
+      rewards[i] = arm1.getReward();
+   }
+   
+   double totalReward = 0.0;
+   
+   for (int i = 0; i < num; i++) {
+      totalReward = totalReward + rewards[i];
+   }
+   
+   totalReward = totalReward / num;
+   
+   cout << "Found meaen: " << totalReward << endl;
+   
+    return 0;
+}
