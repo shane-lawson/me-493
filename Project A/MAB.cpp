d5024530303e2b2b89cd224aa1467c7cd8e5c17b
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-08 22:36:43 -0800

    Create multi-armed bandit placeholder

---
+++ Project A/MAB.cpp
@@ -0,0 +1,9 @@
+//
+//  MAB.cpp
+//  ME 493
+//
+//  Created by Shane Lawson on 2/8/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include "MAB.h"
---
+++ Project A/MAB.h
@@ -0,0 +1,18 @@
+//
+//  MAB.h
+//  ME 493
+//
+//  Created by Shane Lawson on 2/8/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#ifndef MAB_h
+#define MAB_h
+
+#include "Arm.h"
+
+class MAB {
+   
+};
+
+#endif /* MAB_h */
