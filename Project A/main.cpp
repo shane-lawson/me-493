b295fdf17562e92f27d0fa3bc6961e78a447ad32
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-11 18:17:51 -0800

    Add file output for testing
    Adding file output and random testing then plotting in MATLAB to
    visually check everything is working well.

--- Project A/main.cpp
+++ Project A/main.cpp
@@ -9,22 +9,38 @@
 #include "MAB.h"
 #include "Agent.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
    // insert code here...
    cout << "Magical butterflies and unicorns." << endl;
 
-   int num = 1000000000;
-   int numArms = 5;
+   MAB steve(1);
    
-   MAB mab(numArms);
-   
-   Agent Bond(&mab);
-   
-   for (int i = 0; i < num; i++) {
-      Bond.executeCycle();
+   Agent Bourne(&steve);
+
+   ofstream fout;
+
+   for (int i = 0; i < 30; i++) {
+      Bourne.testA(1000000,0.01);
    }
+   
+//   
+//   int num = 1000000;
+//   int numArms = 12;
+//   
+//   MAB mab(numArms);
+//   
+//   Agent Bond(&mab);
+//   
+//   fout.open("rewards12.txt");
+//   
+//   for (int i = 0; i < num; i++) {
+//      Bond.executeCycle();
+//      Bond.showValues(&fout);
+//   }
+//   fout.close();
    
    return 0;
 }
