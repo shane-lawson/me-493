6e6b708ca046df2931ae1cc1263477d23706b7fb
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-19 11:42:49 -0800

    Add main (placeholder)

---
+++ HW 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW 2
+//
+//  Created by Shane Lawson on 2/19/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+   // insert code here...
+   std::cout << "Magical butterflies and unicorns." << endl;
+    return 0;
+}
