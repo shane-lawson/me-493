39cb17ee9e7c68adbd6cca185d65f80d6baecfcd
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-26 08:50:15 -0800

    Initial commit

---
+++ HW 2/Agent.cpp
@@ -0,0 +1,9 @@
+//
+//  Agent.cpp
+//  ME 493
+//
+//  Created by Shane Lawson on 2/26/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include "Agent.h"
---
+++ HW 2/Agent.h
@@ -0,0 +1,13 @@
+//
+//  Agent.h
+//  ME 493
+//
+//  Created by Shane Lawson on 2/26/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#ifndef Agent_h
+#define Agent_h
+
+
+#endif /* Agent_h */
