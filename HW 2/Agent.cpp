3728217410978b890fe0566f9a377f7a7ee2d39c
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-27 21:51:57 -0800

    Add manual and auto move

--- HW 2/Agent.cpp
+++ HW 2/Agent.cpp
@@ -12,6 +12,8 @@
 
 #include <iostream>
 
+#include <curses.h>
+
 Agent::Agent() {
    //construct an agent
    //seed random numbers only once when class is first initialized
@@ -21,9 +23,58 @@ Agent::Agent() {
       seeded = true;
    }
 
-   Goal endGoal(rand()%map.getNumCols(),rand()%map.getNumRows());
+   endGoal = Goal(rand()%map.getNumCols(),rand()%map.getNumRows());
 
    //place agent in random spot
    pos.x = rand()%map.getNumCols();
    pos.y = rand()%map.getNumRows();
+   std::cout << "x: " << pos.x << "\ty: " << pos.y << std::endl;
+}
+
+void Agent::move(char direction) {
+   switch (direction) {
+      case 'u':
+         if (pos.y-1 >= 0) {
+            pos.y--;
+         }
+         break;
+      case 'd':
+         if (pos.y+1 < map.getNumRows()) {
+            pos.y++;
+         }
+         break;
+      case 'l':
+         if (pos.x-1 >= 0) {
+            pos.x--;
+         }
+         break;
+      case 'r':
+         if (pos.x+1 < map.getNumCols()) {
+            pos.x++;
+         }
+         break;
+         
+      default:
+         break;
+   }
+//   std::cout << "x: " << pos.x << "\ty: " << pos.y << std::endl;
+   map.displayGrid(pos.x, pos.y, endGoal.getPosition().x, endGoal.getPosition().y);
+}
+
+void Agent::move() {
+   Position goalPos = endGoal.getPosition();
+   std::cout << goalPos.x << "\t" << goalPos.y << std::endl;
+   if (goalPos.y != pos.y) {
+      if (goalPos.y - pos.y > 0) {
+         this->move('d');
+      } else {
+         this->move('u');
+      }
+   } else {
+      if (goalPos.x - pos.x > 0) {
+         this->move('r');
+      } else {
+         this->move('l');
+      }
+   }
 }
