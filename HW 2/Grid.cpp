2b7920f2a67f33b7022b01d3b7b9b234d3c91f86
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-27 23:05:54 -0800

    Add comments

--- HW 2/Grid.cpp
+++ HW 2/Grid.cpp
@@ -15,6 +15,7 @@ Grid::Grid() {
 }
 
 void Grid::getRowsAndColsFromUser(int& row, int& column) {
+   //clear screen
    std::cout << std::string(75,'\n');
    std::cout << "Please specify grid size." << std::endl;
    std::cout << "Rows: ";
@@ -35,17 +36,23 @@ void Grid::displayGrid(int xAgent, int yAgent, int xGoal, int yGoal) {
    for (int i = -1; i < rows; i++) {
       for (int j = -1; j < columns; j++) {
          if (j==-1 || i==-1) {
+            //if at a wall position, show wall
             std::cout << "#";
          } else if (j==xAgent && i==yAgent) {
+            //if at agent position, show agent
             std::cout << "A";
          } else if (j==xGoal && i==yGoal) {
+            //if at goal position, show goal
             std::cout << "G";
          }else {
+            //everywhere else, show empty space
             std::cout << " ";
          }
       }
+      //finish right wall
       std::cout << "#" << std::endl;
    }
+   //finish bottom wall
    for (int i = -1; i <= columns; i++) {
       std::cout << "#";
    }
--- HW 2/Grid.h
+++ HW 2/Grid.h
@@ -9,16 +9,16 @@
 #ifndef Grid_h
 #define Grid_h
 
-class Grid {
-   int rows;
-   int columns;
+class Grid { //not fully implemented for Project B
+   int rows;                               //number of rows
+   int columns;                            //number of columns
    
-   void getRowsAndColsFromUser(int&,int&);
+   void getRowsAndColsFromUser(int&,int&); //prompts user for size of grid and updates values by reference
 public:
-   Grid();
-   int getNumRows();
-   int getNumCols();
-   void displayGrid(int,int,int,int);
+   Grid();                                 //constructs a grid
+   int getNumRows();                       //returns the number of rows contained in the grid
+   int getNumCols();                       //returns the numer of columns contained in the grid
+   void displayGrid(int,int,int,int);      //outputs the grid, agent position, and goal position to the console
 };
 
 #endif /* Grid_h */
