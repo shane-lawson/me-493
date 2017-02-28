00db0703bb60d28702243e437db47d0a5a4e0c6d
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-27 22:52:37 -0800

    Break program up into functions

--- HW 2/main.cpp
+++ HW 2/main.cpp
@@ -12,54 +12,92 @@
 #include "Agent.h"
 using namespace std;
 
+int promptForControlMode();
+void runManual(Agent*);
+void runAuto(Agent*);
+
+////////// MAIN //////////
+
 int main() {
    int controlMode = -1;
+   
+   //create agent
    Agent Bond;
+   
+   //get control mode from user
+   controlMode = promptForControlMode();
+
+   if (controlMode == 1) {
+      runManual(&Bond);
+   } else {
+      runAuto(&Bond);
+   }
+   
+   return 0;
+}
+
+////////// FUNCTIONS //////////
+
+void runManual(Agent* agent) {
+   char input = 'a';
+   char done = false;
+   
+   //manual control
+   while(!done) {
+      //clear screen and print instructions
+      cout << string(75,'\n');
+      cout << "Enter WASD to move." << endl;
+      cout << "Enter 'q' or 'Q' to quit." << endl;
+      
+      //move agent based on user input
+      done = agent->move(input);
+      if (!done) {
+         //prompt for next move
+         cout << endl << "Enter next move(s): ";
+         cin >> input;
+         if (input == 'q' || input == 'Q') {
+            break;
+         }
+      }
+   }
+}
+
+void runAuto(Agent* agent) {
+   char input = 'a';
+   char done = false;
+   //auto control
+   
+   //clear screen
+   cout << string(75,'\n');
+   
+   //move once to show grid
+   done = agent->move(input);
+   
+   //loop until goal is found
+   while(!done) {
+      //clear screen
+      cout << string(75,'\n');
+      
+      //move in auto mode
+      done = agent->move();
+      if (!done) {
+         cout << endl;
+      }
+      
+      //sleep thread so user can see agent work
+      this_thread::sleep_for (chrono::milliseconds(500));
+   }
+}
+
+int promptForControlMode() {
+   int input;
+   
    cout << string(75,'\n');
    cout << "How would you like to proceed?" << endl;
    cout << "\t1. Manual Control Mode" << endl;
    cout << "\t2. Rule of Thumb Control Mode" << endl;
    cout << "Enter your selection: ";
-   cin >> controlMode;
+   cin >> input;
    
-   char input = 'a';
-   char done = false;
-   if (controlMode == 1) {
-      while(!done) {
-         cout << string(75,'\n');
-         cout << "Enter WASD to move." << endl;
-         cout << "Enter 'q' or 'Q' to quit." << endl;
-         done = Bond.move(input);
-         if (!done) {
-            cout << endl << "Enter next move(s): ";
-            cin >> input;
-            if (input == 'q' || input == 'Q') {
-               break;
-            }
-         }
-      }
-   } else {
-      //clear screen
-      cout << string(75,'\n');
-      
-      //move once to show grid
-      done = Bond.move(input);
-      
-      //loop until goal is found
-      while(!done) {
-         //clear screen
-         cout << string(75,'\n');
-         
-         //move in auto mode
-         done = Bond.move();
-         if (!done) {
-            cout << endl;
-         }
-
-         //sleep thread so user can see agent work
-         this_thread::sleep_for (chrono::milliseconds(500));
-      }
-   }
-   
-   return 0;
+   return input;
 }
