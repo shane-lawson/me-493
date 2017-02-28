34f627137fa6e4c2bce448a9b06a36d06b55e039
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-02-27 22:40:23 -0800

    Add all the things, now working

--- HW 2/main.cpp
+++ HW 2/main.cpp
@@ -7,12 +7,59 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "Agent.h"
 using namespace std;
 
 int main() {
-   // insert code here...
-   cout << "Magical butterflies and unicorns." << endl;
+   int controlMode = -1;
    Agent Bond;
-    return 0;
+   cout << string(75,'\n');
+   cout << "How would you like to proceed?" << endl;
+   cout << "\t1. Manual Control Mode" << endl;
+   cout << "\t2. Rule of Thumb Control Mode" << endl;
+   cout << "Enter your selection: ";
+   cin >> controlMode;
+   
+   char input = 'a';
+   char done = false;
+   if (controlMode == 1) {
+      while(!done) {
+         cout << string(75,'\n');
+         cout << "Enter WASD to move." << endl;
+         cout << "Enter 'q' or 'Q' to quit." << endl;
+         done = Bond.move(input);
+         if (!done) {
+            cout << endl << "Enter next move(s): ";
+            cin >> input;
+            if (input == 'q' || input == 'Q') {
+               break;
+            }
+         }
+      }
+   } else {
+      //clear screen
+      cout << string(75,'\n');
+      
+      //move once to show grid
+      done = Bond.move(input);
+      
+      //loop until goal is found
+      while(!done) {
+         //clear screen
+         cout << string(75,'\n');
+         
+         //move in auto mode
+         done = Bond.move();
+         if (!done) {
+            cout << endl;
+         }
+
+         //sleep thread so user can see agent work
+         this_thread::sleep_for (chrono::milliseconds(500));
+      }
+   }
+   
+   return 0;
 }
