ed773cc52f9241a2f00b41e9738da72c5172b67f
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-03-10 11:41:26 -0800

    Initial commit

---
+++ HW 3/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HW 3
+//
+//  Created by Shane Lawson on 3/10/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   // insert code here...
+   cout << "Magical butterflies and unicorns." << endl;
+    return 0;
+}
