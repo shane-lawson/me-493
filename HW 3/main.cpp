ad66e5702ab750c63dd2ae6ce54926ef172ed0d3
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-03-10 13:49:32 -0800

    Add code to test both methods
    Prior to adding the walls.

--- HW 3/main.cpp
+++ HW 3/main.cpp
@@ -7,10 +7,27 @@
 //
 
 #include <iostream>
+#include "Agent.h"
+#include "Grid.h"
+
 using namespace std;
 
 int main() {
    // insert code here...
    cout << "Magical butterflies and unicorns." << endl;
+   Grid gridworld;
+   Agent Bond(&gridworld);
+   
+   //heuristic method
+   for (int i = 0; i < 1000; i++) {
+      Bond.move();
+      Bond.displayGrid();
+   }
+   
+   //q-learner method
+   while (gridworld.getNumTimesFound() < 1) {
+      Bond.runCycle();
+   }
+
     return 0;
 }
