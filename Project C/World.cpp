365d1357a3c9a47331394f85f8364417175391d2
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-03-25 12:23:52 -0700

    Initial commit

---
+++ Project C/World.cpp
@@ -0,0 +1,9 @@
+//
+//  World.cpp
+//  ME 493
+//
+//  Created by Shane Lawson on 3/25/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include "World.h"
---
+++ Project C/World.h
@@ -0,0 +1,16 @@
+//
+//  World.h
+//  ME 493
+//
+//  Created by Shane Lawson on 3/25/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#ifndef World_h
+#define World_h
+
+class World {
+   
+};
+
+#endif /* World_h */
