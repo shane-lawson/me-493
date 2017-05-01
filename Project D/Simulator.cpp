62ea3816f962e0c65cf15773a0996b76fc8e6e19
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-04-30 22:40:31 -0700

    Implement Simulator
    4/28 forgot to commit.

--- Project D/Simulator.cpp
+++ Project D/Simulator.cpp
@@ -7,3 +7,40 @@
 //
 
 #include "Simulator.h"
+#include <cmath>
+
+#include <iostream>
+
+Simulator::Simulator() {
+   x = 1.0;
+   y = 1.0;
+   theta = 0.0;
+   omega = 0.0;
+   u = 0.0;
+}
+
+void Simulator::calc_pos() {
+   x = x + sin(theta)*dt;
+   y = y + cos(theta)*dt;
+}
+
+void Simulator::calc_theta() {
+   theta = theta + omega*dt;
+}
+
+void Simulator::calc_omega() {
+   omega = omega + (u-omega)*(dt/T);
+}
+
+void Simulator::step() {
+   calc_omega();
+   calc_theta();
+   calc_pos();
+   
+   std::cout << "X: " << x << "\t";
+   std::cout << "Y: " << y << "\t";
+   std::cout << "Theta: " << theta << "\t";
+   std::cout << "Omega: " << omega << "\t";
+   std::cout << "u: " << u << "\t";
+   std::cout << std::endl;
+}
--- Project D/Simulator.h
+++ Project D/Simulator.h
@@ -9,7 +9,21 @@
 #ifndef Simulator_h
 #define Simulator_h
 class Simulator {
+   double x;
+   double y;
+   double theta;
+   double omega;
+   double u;
+   const double v = 3.0;
+   const double dt = 0.2;
+   const double T = 5.0;
    
+public:
+   Simulator();
+   void calc_pos();
+   void calc_theta();
+   void calc_omega();
+   void step();
 };
 
 #endif /* Simulator_h */
