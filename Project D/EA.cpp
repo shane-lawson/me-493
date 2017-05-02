a523a1ea961d21b3cd166882ae8d79b9398968ed
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-05-01 23:33:02 -0700

    Implement most of EA

--- Project D/EA.cpp
+++ Project D/EA.cpp
@@ -7,3 +7,60 @@
 //
 
 #include "EA.h"
+#include <random>
+
+EA::EA() {
+   //seed random numbers only once when class is first initialized
+   static bool seeded = false;
+   if(!seeded){
+      srand((int)time(NULL));
+      seeded = true;
+   }
+   
+   for (int i = 0; i < 50; i++) {
+      Weights newWeight;
+      population.push_back(newWeight);
+   }
+}
+
+void EA::evaluate() {
+   
+}
+
+void EA::downselect(std::vector<double> fitnesses) {
+   std::vector<Weights> nextGen;
+   int minPosition = 0;
+   int minFitness = 99999999.9;
+   
+   //save best policy for eventual optimal policy
+   for (int i = 0; i < fitnesses.size(); i++) {
+      if (fitnesses.at(i) < minFitness) {
+         minFitness = fitnesses.at(i);
+         minPosition = i;
+      }
+   }
+   
+   //binary tournament for rest
+   for (int i = 0; i < population.size()/2 -1; i++) {
+      int first = rand()%100;
+      int second = rand()%100;
+      
+      if (fitnesses.at(first) < fitnesses.at(second)) {
+         nextGen.push_back(population.at(first));
+      } else {
+         nextGen.push_back(population.at(second));
+      }
+   }
+   
+   population.clear();
+   population = nextGen;
+}
+
+void EA::replicate() {
+   for (int i = 0; i < population.size(); i++) {
+      Weights mutatedWeights = population.at(i);
+      mutatedWeights.mutate(0.2);
+      
+      population.push_back(mutatedWeights);
+   }
+}
--- Project D/EA.h
+++ Project D/EA.h
@@ -6,10 +6,19 @@
 //  Copyright © 2017 Shane Lawson. All rights reserved.
 //
 
+#include "Weights.h"
+#include <vector>
+
 #ifndef EA_h
 #define EA_h
 class EA {
+   std::vector<Weights> population;
    
+public:
+   EA();
+   void evaluate();
+   void downselect(std::vector<double>);
+   void replicate();
 };
 
 #endif /* EA_h */
