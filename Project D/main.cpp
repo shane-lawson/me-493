fc3515a8b7d647ffaad1e0d54ee80323872fbd28
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-05-02 01:36:10 -0700

    Implement EA stuff

--- Project D/main.cpp
+++ Project D/main.cpp
@@ -8,24 +8,18 @@
 
 #include "EA.h"
 #include "Simulator.h"
-#include "LY_NN.h"
 #include <iostream>
 using namespace std;
 
 int main() {
    cout << "Magical butterflies and unicorns." << endl;
-   std::vector<double> inputs;
-   neural_network NN;
-
-   NN.setup(2, 5, 1);
-   NN.set_in_min_max(-M_PI, M_PI); //theta
-   NN.set_in_min_max(-17.0, 17.0); //omega
-   NN.set_out_min_max(-15.0, 15.0); //u
    
-   Simulator thing;
+   EA ea;
    
-   for (int i = 0; i < 100; i++) {
-      thing.step();
+   for (int i = 0; i < 1; i++) {
+      ea.replicate();
+      ea.evaluate();
+      ea.downselect();
    }
    
    return 0;
