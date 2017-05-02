93d1f103f31885e5f42cdaa78ace2de89c20617c
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-05-01 23:33:23 -0700

    Add neural network

--- Project D/main.cpp
+++ Project D/main.cpp
@@ -14,6 +14,13 @@ using namespace std;
 
 int main() {
    cout << "Magical butterflies and unicorns." << endl;
+   std::vector<double> inputs;
+   neural_network NN;
+
+   NN.setup(2, 5, 1);
+   NN.set_in_min_max(-M_PI, M_PI); //theta
+   NN.set_in_min_max(-17.0, 17.0); //omega
+   NN.set_out_min_max(-15.0, 15.0); //u
    
    Simulator thing;
    
