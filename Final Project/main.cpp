2cb53ea2308ca50ec01f513519c941930eb31187
Shane Lawson <shane-lawson@users.noreply.github.com>
2017-05-10 11:10:13 -0700

    Initial commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Shane Lawson on 5/10/17.
+//  Copyright © 2017 Shane Lawson. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Magical butterflies and unicorns." << endl;
+   
+   return 0;
+}
